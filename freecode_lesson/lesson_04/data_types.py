d0083f98b84ba4eeb9527cfe11d407ed324ce09f
LATEEF-hub <latifkk@krok.edu.ua>
2023-10-15 08:19:55 +0100

    string index values

--- freecode_lesson/lesson_04/data_types.py
+++ freecode_lesson/lesson_04/data_types.py
@@ -17,4 +17,16 @@ print("")
 #Build a Menu
 title = "menu".upper()
 print(title.center(20, '='))
-print("coffee".ljust(16, ".") + "€4".rjust(4))
+print("Coffee".ljust(16, ".") + "€4".rjust(4))
+print("Muffins".ljust(16, ".") + "€12".rjust(4))
+print("Cheesecake".ljust(16, ".") + "€6".rjust(4))
+
+print("")
+
+#String Index Values
+print(first[1]) #second index value
+print(first[-1]) #Last index value
+print(first[1:-1])
+print(first[1:]) 
+print(first[:3])
+print("")
