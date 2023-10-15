3a754fb61149f0d34e1551869a156a51a50f9e31
LATEEF-hub <latifkk@krok.edu.ua>
2023-10-15 07:38:15 +0100

    start to build a menu

---
+++ freecode_lesson/lesson_04/data_types.py
@@ -0,0 +1,20 @@
+##String Data type 
+
+#literal assignemt 
+first = 'Quduis '
+last = 'Lateef'
+fullname = first + last
+print(fullname)
+
+
+#Constructor assignement
+
+fullname = str('Quduis Lateef')
+print(fullname)
+
+print("")
+
+#Build a Menu
+title = "menu".upper()
+print(title.center(20, '='))
+print("coffee".ljust(16, ".") + "€4".rjust(4))
