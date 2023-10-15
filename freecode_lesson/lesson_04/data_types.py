acc53b5e88db9a2dbce1972df599e9ee6d0c15ab
LATEEF-hub <latifkk@krok.edu.ua>
2023-10-15 08:40:27 +0100

    numeric data type

--- freecode_lesson/lesson_04/data_types.py
+++ freecode_lesson/lesson_04/data_types.py
@@ -42,3 +42,26 @@ print(type(z))
 print(myvalue)
 print(isinstance(myvalue, bool))
 
+
+#Numeric Data types
+#INTEGER
+
+num = 10
+my_num = int(20)
+print(type(num))
+print(isinstance(my_num, int))
+
+#FLOAT
+gpa = 2.4
+grade = float(3.5)
+print(type(gpa))
+print(isinstance(grade, float))
+
+#COMPLEX
+comp_value = 4 + 3j
+comp_num = complex(7+8j)
+print(type(comp_num))
+print(comp_value.imag)
+print(comp_value.real)
+
+
