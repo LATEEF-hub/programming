3c173d53830044ad2998ac3a4646f7760f417fd3
LATEEF-hub <latifkk@krok.edu.ua>
2023-10-15 19:40:31 +0100

    add random choice for computer

--- freecode_lesson/lesson_05/rps.py
+++ freecode_lesson/lesson_05/rps.py
@@ -1,10 +1,10 @@
-
-
+import sys
+import random
 
 print("")
-line1 = '********************' #header and footer
-line2 = '*                  *' #wrapper
-line3 = '*     WELCOME      *' #message
+line1 = '********************'  # header and footer
+line2 = '*                  *'  # wrapper
+line3 = '*     WELCOME      *'  # message
 
 print(line1)
 print(line2)
@@ -13,9 +13,19 @@ print(line2)
 print(line1)
 print('')
 
-playerchoice = input("Enter your choice:\n1.Rock,\n2.Paper,or\n3.Scissors\n")
+playerchoice = input("Enter your choice:\n1.Rock,\n2.Paper, or\n3.Scissors\n")
 
-player = int(playerchoice)
+player = int(playerchoice)  # Casting user input to INT
 
 if player < 1 | player > 3:
-    print("Invalid Entry! Please enter a number between 1-3.")
+    # Validating user Input
+    sys.exit("Invalid Entry! Please enter a number between 1-3.") #Here I change to SYS.exit to be able to exit the game
+
+computerchoice = random.choice("123")
+computer = int(computerchoice)
+
+print("")
+print("You chose " + playerchoice + ".")
+print("Python chose " + computerchoice + ".")
+print("")
+# Comparing the choices of both players
